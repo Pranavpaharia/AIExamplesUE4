f014c738e8372ff65555f663e875ed99d81236b9
pranavpaharia <pranavpaharia@gmail.com>
2017-11-03 01:45:33 +0530

    Added source files

---
+++ AIExamplesUE4/Source/AIExamplesUE4/AIExamplesUE4GameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AIExamplesUE4GameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAIExamplesUE4GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAIExamplesUE4GameMode();
+};
+
+
+
